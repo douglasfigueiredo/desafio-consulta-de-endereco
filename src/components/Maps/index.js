c48f9a887a70da0194b2200658f8362a164d674a
Douglas Figueiredo <douglasarts@gmail.com>
2018-11-08 22:01:48 -0200

    Ajustando a dimensão do Mapa pra melhor visualizar o resultado da busca.

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -5,8 +5,8 @@ import ReactGoogleMapImage from 'react-google-map-image';
 const Maps = ({ positionCenter }) => {
   const googleMapApiConfig = {
     center: positionCenter,
-    size: '1024x500',
-    zoom: '16',
+    size: '1024x450',
+    zoom: '15',
     key: 'AIzaSyDQqA7pMadH6_nEbQHzrZrJiwX4ovzsdMM',
     maptype: 'roadmap',
   };
